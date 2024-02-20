9b0d26bfbb0347cf970712743ecaabfeaca19ef8
Geraldo <gsjunior86@gmail.com>
2024-02-20 23:14:25 +0100

    first usage of fileReaderHelper

--- src/main/scala/br/gsj/camara/analise/deputado/GastosAggDeputadoAno.scala
+++ src/main/scala/br/gsj/camara/analise/deputado/GastosAggDeputadoAno.scala
@@ -1,14 +1,20 @@
 package br.gsj.camara.analise.deputado
 
+import br.gsj.camara.helpers.CamaraHelper.DespesaCotaParlamentar
+import br.gsj.camara.helpers.{CamaraDataset, FileReaderHelper}
 import br.gsj.spark.{SparkWorkFlow, WorkFlow}
 
-class GastosAggDeputadoAno extends WorkFlow{
-  override def run(args: Array[String]): Unit = {
+class GastosAggDeputadoAno(fileReaderHelper: FileReaderHelper) extends WorkFlow {
 
+  import spark.implicits._
+
+  override def run(args: Array[String]): Unit = {
+    val gastosDF =
+      fileReaderHelper.readDataSet(CamaraDataset.DespesaCotaParlamentar, Some(2013)).as[DespesaCotaParlamentar]
   }
 }
 
-
-object GastosAggDeputadoAno extends SparkWorkFlow{
-  override def run(args: Array[String]): Unit = new GastosAggDeputadoAno().run(args)
+object GastosAggDeputadoAno extends SparkWorkFlow {
+  override def run(args: Array[String]): Unit =
+    new GastosAggDeputadoAno(new FileReaderHelper(spark)).run(args)
 }
